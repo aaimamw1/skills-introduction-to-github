f8f66cc5b37920b98e29ee9f315a8732a8f8770f
aaimamw1 <aaimamw1@gmail.com>
2025-01-29 23:35:47 -0800

    Add files via upload

---
+++ humpty-dumpty.py
@@ -0,0 +1,10 @@
+s = "Jypu6zRKMYKHj2c1s8uVWuq7KjVanellusvdQvlmmCQ5SwDN0yqKbUrJNfZIviX6Tx4nUmEluoJWz418Jjg9nUT4p27SDkvjpR0lTBM3i6FOidcISVqwDbnWfzJkzDGqPWdDnSM3AmOl0OE2h7up3ggh1docilistudJGlmQPHMxRr."
+a = 26
+b = 33
+c = 153
+d = 159
+slice1 = s[a:b+1]
+slice2 = s[c:d+1]
+result = (slice1 + " " + slice2)
+print= ("result")
+
